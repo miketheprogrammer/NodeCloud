fd18c07acfe4e4c8ba7572c6f416e4295f119f1b
Michael Hernandez <michael.hernandez1988@gmail.com>
2013-08-12 15:01:54 -0400

    Improved Bounce example

--- examples/serverbounce.js
+++ examples/serverbounce.js
@@ -2,13 +2,35 @@ var http = require('http');
 var Bouncy = require('bouncy');
 var Manager = require("../lib/manager");
 
+/*
+  This example creates a server and bounces connections to the best host available.
+
+  Best here is defined by the first child that supports an http server.
+
+  The child node has notified the master of its remote host and port.
+*/
+
+// Lets add a function to the Manager for getting the first child with a remote port or host
+
+Manager.prototype.getFirstAvailableServer = function ( ) {
+    for ( var key in this.node.children ) {
+        var child = this.node.children[key];
+        if ( child.remotePort != undefined )
+            return child;
+    }
+}
+
+
 exports.createServer = function(node, port, host) {
     process.node = node;
-    process.manager = new Manager();
-    process.manager.register(process.node);
+    var manager = new Manager();
+    manager.register(process.node);
     var server = http.createServer();
     server.listen(port);
     Bouncy({server:server},function( req, res, bounce ) {
-        bounce(process.manager.getBest().remotePort);
+        var remoteNode = manager.getFirstAvailableServer();
+        var host = remoteNode.remoteAddress;
+        var port = remoteNode.remotePort;
+        bounce(host, port);
     });
 };
