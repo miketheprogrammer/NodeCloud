08a5ebadee094781dc2721a8012b8b4fcbd19275
Michael Hernandez <michael.hernandez1988@gmail.com>
2013-08-12 16:03:23 -0400

    Fix: remotePort DNOE : Version Bump

--- examples/client.js
+++ examples/client.js
@@ -1,4 +1,7 @@
 var Factory = require('../lib/factory')
-var client = Factory.NodeFactory(null, {client:{}});
+var client = Factory.NodeFactory(null, {client:{
+    host: "10.2.43.55",
+    port: 5555
+}});
 
 
